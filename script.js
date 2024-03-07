16d5f2f144efb96d28a82deca668e00b8edf9adf
SHODya <101045331+SHODya@users.noreply.github.com>
2024-03-08 00:41:13 +0500

    Hello

---
+++ script.js
@@ -0,0 +1,11 @@
+// function changeImage() {
+//     if (document.getElementById("heartVoid").src == "/images/HeartVoid.png"){
+//         document.getElementById("heartVoid").src = "/images/HeartFill.png";
+//     } else {
+//         document.getElementById("heartVoid").src = "/images/HeartVoid.png";
+//     }
+// }
+const heart = document.getElementById("heartVoid");
+function changeImage() {
+    heart.classList.add("clicked");
+}
